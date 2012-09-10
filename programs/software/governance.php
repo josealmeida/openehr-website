141e14fb72ca787befbd85b814c6ac8de0385f11
Erik Sundvall <erik@urgott.se>
2012-09-10 13:19:44 +0300

    Update programs/software/governance.php

--- programs/software/governance.php
+++ programs/software/governance.php
@@ -8,7 +8,7 @@ $pageOn = basename($_SERVER['PHP_SELF']);
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">	
 	<meta http-equiv="Content-Language" content="en">
 
-	<title>openEHR - Software Program</title>
+	<title>openEHR - Software Program - Governance</title>
 
 	<meta name="description" content="openEHR provides open source specifications and reference implementations of future proof EHR systems">
 	<meta name="keywords" content="archetypes, openEHR, EHR, electronic health record, electronic medical record, ADL, reference model, clinical models, healthcare, health informatics, medical informatics, EHR standards, ISO 18308, CEN 13606, health care software, open source software, knowledge modelling, patient-centric">
@@ -57,7 +57,7 @@ $pageOn = basename($_SERVER['PHP_SELF']);
 
 		<div id="TextArea">
 
-		<h1>Governance</h1>
+		<h1>Governance & Members</h1>
 		<p>The governance structure of the openEHR software program/group is mosly mail-list based
 		   and inspired by the <a href="http://www.apache.org/foundation/">Apache Software Foundation</a>
 		   and <a href="http://www.apache.org/foundation/how-it-works.html#management">the way it works</a>.
@@ -67,11 +67,32 @@ $pageOn = basename($_SERVER['PHP_SELF']);
 		
 		<h2>Mailing list and Program Qualified Members</h2>
 		
-		<p>The majority of the work and technical decisions is discussed on the 
+		<p>The majority of the work and software decisions should be discussed on the 
 		   <a href="/community/mailinglists.php">openehr-implementers mailing list</a>
-		   where anybody can contribute to the concensus-targeted discussion, and if voting is needed then
+		   where anybody can contribute to the concensus-targeted discussion (no qualified membership required).
+		</p>
+		<p>
+		   If voting is needed then
 		   votes from <em>Program Qualified Members</em> will be evaluated using <a 
-		   href="http://www.apache.org/foundation/voting.html" Apache style voting</a>.
+		   href="http://www.apache.org/foundation/voting.html" Apache style voting and interpretation</a>.
+		</p>
+		<p>Qualified Membership should be based on meritocracy and willingness to contribute. 
+		   Qualified Membership is not time-limited. Qualified Membership can be ended if the 
+		   member requests to be removed.
+		</p> 
+		<p> 
+		   If three independent* <em>Qualified Members</em> agree to suggest a 
+		   new member and presents the candidate (including openEHR related merits), then the <em>Program Committee</em>
+		   should discuss and consider if the new member should be a) approved at present or b) be recomended to first 
+		   gain more experience in order to get suggested again later. 
+		</p> 
+		<p>   
+		   If thorny vendor/institution-related issues arise that at least three independent* Qualified 
+		   Members think would benefit from vendor/institution balance, then the issue can be transferred
+		   to the program committiee instead.
+		</p> 
+		<p>
+		   *) Independent in this case refers to not being dependent on the same organisation for salary or other benefits.
 		</p>
 		
 		<h3>Curent Program Qualified Members</h3>
@@ -95,17 +116,34 @@ $pageOn = basename($_SERVER['PHP_SELF']);
 		
 		
 		<h2>Program Committee</h2>
+		
 		<p>The <em>Program Committee</em> is according to openEHR foundation 
 		<a href="https://sites.google.com/a/openehrfoundation.org/foundation/home/foundation-committees">program rules</a>
-		limited to a maximum of 9 and minimum of 5 Qualified Members. During committee formation we have strived
+		limited to a maximum of 9 and minimum of 5 Qualified Members. During initial/bootstrap committee formation we have strived
 		to set it up in a way so that no specific company or institution is represented more than
 		once (since the number of seats is limited).
 		</p>
 		
+		<p>The <em>Program Committee</em> positions should be renewed or changed on a regular basis 
+		as decided by the openEHR foundation. The <em>Software Program Qualified Members</em> suggest 
+		new and/or renewed positions in the <em>Software Program Committee</em> to the openEHR foundation
+		board that does final approval (or asks the <em>Software Program Qualified Members</em> for a new suggestion).
+		</p> 
+		
+		<p>The <em>Program Committee</em> is responsible for reporting to and relating to the openEHR foundation board
+		and it also maintins the official (and incubating) openEHR software project listings and approves new 
+		<em>Program Qualified Member</em> suggestions (as described above).	
+		</p>
+		
 		<h3>Curent Program Committee Members</h3>
 		<ul>
-			<li>...</li>
-		</ul>
+			<li>Alan James</li>
+			<li>Diego Bosca</li>
+			<li>Erik Sundvall (coordinator)</li>
+			<li>Pablo Pazos</li>
+			<li>Rong Chen</li>
+			<li>Seref Arikan</li>
+		</ul>		
 		
 		</div>
 			
